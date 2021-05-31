69821dcf9947b372175ebd83c8795454b52c3fed
Monika M <monikamostek23@gmail.com>
2021-05-31 19:00:22 +0200

    Ending game, showing who won and adding data to databe - table game

--- Messages.cs
+++ Messages.cs
@@ -10,6 +10,9 @@
             public const string Start = "###__Start";
             public const string Matches = "###__Matches";
             public const string Logged = "###__Logged";
+            public const string Winner = "###__Winner";
+            public const string SaveGame = "###__SaveGame";
+            public const string Lost = "###__Lost";
         }
 
         public class Client
@@ -21,6 +24,8 @@
             public const string Exit = "###__Exit";
             public const string Login = "###__Login";
             public const string Register = "###__Register";
+            public const string EndGame = "###__EndGame";
+            public const string SaveGame = "###__SaveGame";
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace TCPserver
         public static List<Player> connectedClients = new List<Player>();
         public static List<Match> matches = new List<Match>();
         static SimpleTcpServer server = new SimpleTcpServer("127.0.0.1:8001");
-        public static MankalaDBDataContext db = new MankalaDBDataContext(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\ReEdix\source\repos\TCPserver\databaseM.mdf; Integrated Security = True");
+        public static MankalaDBDataContext db = new MankalaDBDataContext(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\user\source\repos\MankalaTCPserver\databaseM.mdf; Integrated Security = True");
         static void Main(string[] args)
         {         
             server.Events.ClientConnected += Events_ClientConnected;
@@ -102,6 +102,38 @@ namespace TCPserver
                     addUser(Encoding.UTF8.GetString(e.Data));
                     server.Send(e.IpPort, Messages.Server.Disconnect);
                     break;
+
+                case Messages.Client.EndGame:  // endgame message + playercolor
+                    Match thisMatch = matches.Find(x => x.playerWhite == e.IpPort);
+                    if (thisMatch == null)
+                    {
+                        thisMatch = matches.Find(x => x.playerBlack == e.IpPort);
+                    }
+                    Player thisPlayerWhite = connectedClients.Find(x => x.ip == thisMatch.playerWhite);
+                    Player thisPlayerBlack = connectedClients.Find(x => x.ip == thisMatch.playerBlack);
+                    if (messageData[1] == "BLACK")
+                    {
+                        server.Send(e.IpPort, Messages.Server.Winner + ":" + thisPlayerWhite.name + ":" +
+                            thisPlayerBlack.name + ":" + thisPlayerBlack.name);
+                        server.Send(thisMatch.playerWhite, Messages.Server.Lost + ":" + thisPlayerBlack.name);
+
+                    }
+                    else
+                    {
+                        server.Send(e.IpPort, Messages.Server.Winner + ":" + thisPlayerWhite.name + ":" +
+                            thisPlayerBlack.name + ":" + thisPlayerWhite.name);
+                        // do kogo, imie wygranego
+                        server.Send(thisMatch.playerBlack, Messages.Server.Lost + ":" + thisPlayerWhite.name);
+                    }
+                    break;
+
+                case Messages.Client.SaveGame:
+                    Player thisPlayer1 = connectedClients.Find(x => x.name == messageData[1]);
+                    Player thisPlayer2 = connectedClients.Find(x => x.name == messageData[2]);
+                    Player thisPlayer3 = connectedClients.Find(x => x.name == messageData[3]);
+                    addGame(thisPlayer1.name + ":"+ thisPlayer2.name + ":"+ thisPlayer3.name);
+                    break;
+
                 default:
                     break;
             }
@@ -157,6 +189,49 @@ namespace TCPserver
             {
                 Console.WriteLine("Can't submit changes in database - user");
                 Console.WriteLine(e.Message);
+            }
+        }
+        private static void addGame(String gameData)
+        {
+            //player1_name:player2_name:winner_name
+            Game newGame = new Game();
+
+            String[] substring = gameData.Split(':');
+            int p1 = 0, p2 = 0, w = 0;
+
+            foreach(User u in db.Users)
+            {
+                if (u.name == substring[0])
+                {
+                    p1 = u.Id;
+                }
+                if (u.name == substring[1])
+                {
+                    p2 = u.Id;
+                }
+                if (u.name == substring[2])
+                {
+                    w = u.Id;
+                }
+
+            }
+
+            newGame.player1 = p1;
+            newGame.player2 = p2;
+            newGame.winner = w;
+
+            Console.WriteLine("Zapisane osoby to ->" + p1 + ":" + p2 + ":" + w);
+
+            db.Games.InsertOnSubmit(newGame);
+            try
+            {
+                db.SubmitChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Can't submit changes in database - game");
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
             }
         }
     }
